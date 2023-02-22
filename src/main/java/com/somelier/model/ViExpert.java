3f86da73ccf618dc04f3b042f10dc7e22e9f8f01
FHP187 <gjimenez@jaumebalmes.net>
2023-02-22 20:38:26 +0100

    commit exercicis uf3 map/reduce/filter

--- src/main/java/com/somelier/model/ViExpert.java
+++ src/main/java/com/somelier/model/ViExpert.java
@@ -1,4 +1,10 @@
 package com.somelier.model;
 
+import java.util.ArrayList;
+
 public class ViExpert {
+
+    ArrayList<String> Vins = new ArrayList<String>();
+    // Añade el elemento al ArrayList
+
 }
--- src/main/java/com/somelier/model/Vins.java
+++ src/main/java/com/somelier/model/Vins.java
@@ -1,4 +1,58 @@
 package com.somelier.model;
 
+import java.util.ArrayList;
+
 public class Vins {
+
+    private String marca;
+    private String tipus;
+    private ArrayList<Vins> Vinacoteca;
+
+    public Vins(String marca, String tipus) {
+        this.marca = marca;
+        this.tipus = tipus;
+    }
+
+
+    // Getters
+    public String getmarca() {
+        return marca;
+    }
+
+    public String gettipus() {
+        return tipus;
+    }
+
+
+    // Setters
+    public void setmarca(String marca) {
+        this.marca = marca;
+    }
+
+
+    public void settipus(String tipus) {
+        this.tipus = tipus;
+    }
+
+    public void main(String[] args) {
+        Vinacoteca = new ArrayList();
+        Vins v1 = new Vins("Familia Torres","negre");
+        Vins v2 = new Vins("Catena","negre");
+        Vins v3 = new Vins("Vega Sicilia","rosat");
+        Vins v4 = new Vins("Henschke","blanc");
+        Vins v5 = new Vins("Concha y Toro","rosat");
+        Vins v6 = new Vins("Penfolds","negre");
+        Vins v7 = new Vins("Domaine de la Romanée-Conti","rosat");
+        Vins v8 = new Vins("CVNE","blanc");
+        Vins v9 = new Vins("Antinori","blanc");
+        Vins v10 = new Vins("Château Musar","negre");
+        Vins v11 = new Vins("E. Guigal","rosat");
+        Vins v12 = new Vins("Château Lafite","rosat");
+
+        System.out.println(Vinacoteca);
+
+    }
 }
+
+
+
